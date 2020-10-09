68c4119b7f9f3001892c5a82664a83c91b695e80
Kody Dudley <kodymdudley@gmail.com>
2020-10-09 15:07:35 -0600

    edits

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 let pizza = 0
 
 let clickUpgrades = {
-  pizzaCutter: {
+  pizzaCutters: {
     price: 50,
     quantity: 0,
     multiplier: 1,
   },
-  pizzaBox: {
+  pizzaBoxes: {
     price: 500,
     quantity: 0,
     multiplier: 10,
@@ -14,21 +14,76 @@ let clickUpgrades = {
 }
 
 let autoUpgrades = {
-  pizzaServer: {
+  pizzaServers: {
     price: 250,
     quantity: 0,
     multiplier: 5,
   },
-  deliveryDriver: {
+  deliveryDrivers: {
     price: 5000,
     quantity: 0,
     multiplier: 25,
   }
 }
 
-function buyPizzaCutter() {
-
+// UDGRADE BUTTONS SECTION
+function buyPizzaCutters() {
+  clickUpgrades.pizzaCutters.quantity++
+  updatePizzaCutters()
 }
+
+function updatePizzaCutters() {
+  if (pizza >= 50) {
+    let pizzaCuttersElem = document.getElementById("pizzaCuttersValue")
+    pizzaCuttersElem.innerText = `Pizza Cutters: ${clickUpgrades.pizzaCutters.quantity}`
+    pizza -= 50
+    update()
+  }
+}
+
+function buyPizzaBoxes() {
+  clickUpgrades.pizzaBoxes.quantity++
+  updatePizzaBoxes()
+}
+
+function updatePizzaBoxes() {
+  if (pizza >= 500) {
+    let pizzaBoxesElem = document.getElementById("pizzaBoxesValue")
+    pizzaBoxesElem.innerText = `Pizza Boxes: ${clickUpgrades.pizzaBoxes.quantity}`
+    pizza -= 500
+    update()
+  }
+}
+
+function buyPizzaServers() {
+  autoUpgrades.pizzaServers.quantity++
+  updatePizzaServers()
+}
+
+function updatePizzaServers() {
+  if (pizza >= 250) {
+    let pizzaServersElem = document.getElementById("pizzaServersValue")
+    pizzaServersElem.innerText = `Pizza Servers: ${autoUpgrades.pizzaServers.quantity}`
+    pizza -= 250
+    update()
+  }
+}
+
+function buyDeliveryDrivers() {
+  autoUpgrades.deliveryDrivers.quantity++
+  updateDeliveryDrivers()
+}
+
+function updateDeliveryDrivers() {
+  if (pizza >= 5000) {
+    let deliveryDriversElem = document.getElementById("deliveryDriversValue")
+    deliveryDriversElem.innerText = `Delivery Drivers: ${autoUpgrades.deliveryDrivers.quantity}`
+    pizza -= 500
+    update()
+  }
+}
+
+
 
 
 function getPizza() {
@@ -41,5 +96,8 @@ function update() {
   let pizzaElem = document.getElementById("pizza")
   pizzaElem.innerText = `Pieces of Pizza: ${pizza}`
 }
-
 update()
+updatePizzaCutters()
+updatePizzaBoxes()
+updatePizzaServers()
+updateDeliveryDrivers()
